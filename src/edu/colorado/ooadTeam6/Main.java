8c40ff4a8346c806afc3854ce8e188d029294075
JarekTC <jarek.carlson@outlook.com>
2021-02-04 12:15:52 -0700

    Copying over updates from old repo to Main.java and Ship.java

--- src/edu/colorado/ooadTeam6/Main.java
+++ src/edu/colorado/ooadTeam6/Main.java
@@ -1,11 +1,10 @@
 package edu.colorado.ooadTeam6;
-  
+
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-         Ship ship = new Ship();
-         ship.show();
-
-    }
+  public static void main(String[] args) {
+    // write your code here
+    Ship ship = new Ship();
+    ship.show();
+  }
 }
--- src/edu/colorado/ooadTeam6/Ship.java
+++ src/edu/colorado/ooadTeam6/Ship.java
@@ -2,12 +2,17 @@ package edu.colorado.ooadTeam6;
 // This is the  baseclass for your ship.  Modify accordingly
 // TODO: practice good OO design
 public class Ship {
-    private String name;
-    // TODO: create appropriate getter and setter methods
-    // TODO: Understand encapsulation
-    // TODO: Understand what these todo comments mean
+  private String name;
+  // TODO: create appropriate getter and setter methods
+  // TODO: Understand encapsulation
+  // TODO: Understand what these todo comments mean
 
-    public  void show()     {     // dunno why this is here maybe it is just an example method
-        System.out.println("IF you can't see this then something is severely wrong!!");
-    }
+  public void show() { // dunno why this is here maybe it is just an example method
+    System.out.println("IF you can't see this then something is severely wrong!!");
+  }
+
+  public Ship() {
+    // Team Apple, pair 1 was here.
+    // Team6 Pair2 was
+  }
 }
