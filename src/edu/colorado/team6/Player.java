362d9a63b45b1a162974fd90e5c5680cd517745c
Stefan Primelles <Stefan@CyberMente.local>
2021-02-13 22:53:46 -0600

    create constructor and getter for player class

--- src/edu/colorado/team6/Player.java
+++ src/edu/colorado/team6/Player.java
@@ -1,4 +1,14 @@
 package edu.colorado.team6;
 
 public class Player {
+    private String name;
+    private int score = 0;
+    //constructor
+    public Player(String name) {
+        this.name = name;
+    }
+    //getter
+    public String getName() {
+        return name;
+    }
 }
