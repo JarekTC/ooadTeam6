c7ec3c0251eecb9061fcd7efb01f98083a2c2052
Neel Karsanbhai <nkarsanb7682@gmail.com>
2021-02-13 21:40:06 -0700

    Add Player testing class, and test for Player class constructor

---
+++ src/test/edu/colorado/team6/PlayerTest.java
@@ -0,0 +1,15 @@
+package edu.colorado.team6;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+    private Player p;
+
+    @BeforeEach
+    public void setUp() {
+        p = new Player();
+    }
+
+}
