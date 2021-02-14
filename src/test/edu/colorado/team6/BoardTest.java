1c28c25a7040d15a6ffa2f7f1f8a51f47e0c2a6e
JarekTC <jarek.carlson@outlook.com>
2021-02-13 17:23:00 -0700

    Create initial board constructor test

--- src/test/edu/colorado/team6/BoardTest.java
+++ src/test/edu/colorado/team6/BoardTest.java
@@ -1,7 +1,10 @@
 package edu.colorado.team6;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
-
+    @Test
+    public void canMakeBoard() { new Board(); }
 }
