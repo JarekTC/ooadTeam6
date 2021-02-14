cdac67c1edd899f6cecf06c1dc94164f41624f25
Neel Karsanbhai <nkarsanb7682@gmail.com>
2021-02-13 22:01:59 -0700

    Add tests for Player constructor, and Player name setter

--- src/test/edu/colorado/team6/PlayerTest.java
+++ src/test/edu/colorado/team6/PlayerTest.java
@@ -1,15 +1,22 @@
 package edu.colorado.team6;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
     private Player p;
+    private String name = "Neefan";
 
     @BeforeEach
     public void setUp() {
-        p = new Player();
+        p = new Player(name);
+    }
+
+    @Test
+    public void testSetName() {
+        assertEquals(name, p.getName());
     }
 
 }
