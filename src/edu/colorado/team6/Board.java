02f0dbd2336cd620f063c688df9fb3c8f2db50a3
Neel Karsanbhai <nkarsanb7682@gmail.com>
2021-02-13 19:46:16 -0700

    Add getter and setter for board array in Board. Add tests for Board constructor, and getter

--- src/edu/colorado/team6/Board.java
+++ src/edu/colorado/team6/Board.java
@@ -3,8 +3,14 @@ package edu.colorado.team6;
 public class Board {
     private int[][] board = new int[10][10];
 
-
     public Board(int[][] board) {
+        this.board = board;
+    }
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[][] board) {
         this.board = board;
     }
 }
--- src/test/edu/colorado/team6/BoardTest.java
+++ src/test/edu/colorado/team6/BoardTest.java
@@ -1,10 +1,21 @@
 package edu.colorado.team6;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    private Board b;
+
+    @BeforeEach
+    public void setUp() {
+        int[][] board = new int[10][10];
+        b = new Board(board);
+    }
+
     @Test
-    public void canMakeBoard() { new Board(); }
+    public void testGetBoard() {
+        b.getBoard();
+    }
 }
