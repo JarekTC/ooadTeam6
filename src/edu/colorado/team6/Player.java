f0c27b474f2da4d178f23bfce02185696f45ca2f
Stefan Primelles <Stefan@CyberMente.local>
2021-02-17 16:44:12 -0600

    reformat Player class

--- src/edu/colorado/team6/Player.java
+++ src/edu/colorado/team6/Player.java
@@ -1,4 +1,5 @@
 package edu.colorado.team6;
+
 import java.lang.Math;
 
 public class Player {
@@ -6,21 +7,23 @@ public class Player {
     int x;
     int y;
     String hitMiss;
-    Record(int x, int y, String hitMiss){
+
+    Record(int x, int y, String hitMiss) {
       this.x = x;
       this.y = y;
       this.hitMiss = hitMiss;
     }
 
-    public int[] get_Coordinates(){
+    public int[] get_Coordinates() {
       int arr[] = {this.x, this.y};
       return arr;
     }
-    public String get_hitMiss(){
+
+    public String get_hitMiss() {
       return hitMiss;
     }
 
-    public void set_hitMiss(String hitMiss){
+    public void set_hitMiss(String hitMiss) {
       this.hitMiss = hitMiss;
     }
   }
@@ -28,7 +31,7 @@ public class Player {
   private String name;
   private Record record;
   private int score = 0;
-//  private int Record
+  //  private int Record
   private int[][] board = new int[10][10];
 
   // constructor
@@ -49,7 +52,7 @@ public class Player {
     this.score = score;
   }
 
-  public int getPosition(int x, int y){
+  public int getPosition(int x, int y) {
     return this.board[y][x];
   }
 
@@ -57,22 +60,21 @@ public class Player {
   // come back later to verify length of ship matches dist between points
   // Also, prevent overlapping ships
   public int setShip(int x1, int y1, int x2, int y2, int label) {
-    if(label != 0 && label != 1){
+    if (label != 0 && label != 1) {
       System.out.println("Label must be a 1(ship), or a 0(sea)");
       return -1;
     }
-    if((Math.abs(x1-x2) != 0) && (Math.abs(y1-y2) != 0)){
+    if ((Math.abs(x1 - x2) != 0) && (Math.abs(y1 - y2) != 0)) {
       System.out.println("Cannot place ships diagonally!");
       return -1;
     }
-    //adapted for cartesian coordinates
-    if (Math.abs(x1-x2) != 0){
-      for(int i = x1; i <= x2; i++){
+    // adapted for cartesian coordinates
+    if (Math.abs(x1 - x2) != 0) {
+      for (int i = x1; i <= x2; i++) {
         this.board[y1][i] = label;
       }
-    }
-    else{
-      for(int i = y1; i <= y2; i++){
+    } else {
+      for (int i = y1; i <= y2; i++) {
         this.board[i][x1] = label;
       }
     }
@@ -81,7 +83,7 @@ public class Player {
 
   public int hit(int x, int y, Player enemy) {
     int hitStat;
-    hitStat = enemy.getPosition(x,y);
+    hitStat = enemy.getPosition(x, y);
     if (hitStat == 1) {
       System.out.println("Ship hit!");
       return hitStat;
