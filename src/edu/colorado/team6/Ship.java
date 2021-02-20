242ae7def1253878cc34ba3f037a382f70209332
JarekTC <jarek.carlson@outlook.com>
2021-02-20 15:07:21 -0700

    Format Ship class to match google java standards

--- src/edu/colorado/team6/Ship.java
+++ src/edu/colorado/team6/Ship.java
@@ -3,26 +3,26 @@ package edu.colorado.team6;
 
 public class Ship {
 
-    //basic private member variables
+    // Basic private member variables
     private String typeName;
     private static int health;
 
-    //constructor, currently initialized with name of subclass type
+    // Constructor, currently initialized with name of subclass type
     public Ship(String typeOfShip) {
         typeName = typeOfShip;
     }
 
-    //helper function for subclasses to set proper starting health
+    // Helper function for subclasses to set proper starting health
     public static void initializeCorrectHealth(int startHealth) {
         health = startHealth;
     }
 
-    //decrement ship health
+    // Decrement ship health
     public static void takeDamage() {
         if (health != 0) health = health - 1;
     }
 
-    //displays subclass type name passed into constructor
+    // Displays subclass type name passed into constructor
     public void showShipType() {
         System.out.println(typeName);
     }
