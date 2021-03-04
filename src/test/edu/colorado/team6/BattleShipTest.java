a6b8acda835d989fb77eb4aba953e2fb4d1d2f83
Isaac Vance <isaacchristophervance@gmail.com>
2021-03-04 15:29:41 -0700

    Fix minesweeper and battleship tests

--- src/test/edu/colorado/team6/BattleShipTest.java
+++ src/test/edu/colorado/team6/BattleShipTest.java
@@ -13,32 +13,34 @@ class BattleShipTest {
     @Test
     public void canTakeDamage() {
         BattleShip b1 = new BattleShip();
-        b1.takeDamage(1);
+        b1.initializeCorrectHealth(3);
+        assertEquals(0, b1.takeDamage(1));
     }
 
     @Test
     public void initializeHealthTest() {
         BattleShip b2 = new BattleShip();
-        b2.initializeCorrectHealth(4);
+        assertEquals(3, b2.initializeCorrectHealth(3));
     }
 
     @Test
     public void canDisplayTypeName() {
         BattleShip b3 = new BattleShip();
-        b3.showShipType();
+        assertEquals(Constants.BATTLESHIP, b3.showShipType());
     }
 
     @Test
     public void canViewHealth() {
         BattleShip b4 = new BattleShip();
-        b4.getShipHealth();
+        b4.initializeCorrectHealth(3);
+        assertEquals(3, b4.getShipHealth());
     }
 
     @Test
     public void hasCaptainsQuarters(){
         BattleShip b5 = new BattleShip();
         b5.initializeCorrectHealth(3);
-        b5.setCaptainsQuarters(1);
+        assertEquals(1, b5.setCaptainsQuarters(1));
     }
 
     @Test
@@ -46,7 +48,7 @@ class BattleShipTest {
         BattleShip b6 = new BattleShip();
         b6.initializeCorrectHealth(3);
         b6.setCaptainsQuarters(1);
-        b6.getCaptainsQuarters();
+        assertEquals(1, b6.getCaptainsQuarters());
     }
 
     @Test
@@ -55,7 +57,8 @@ class BattleShipTest {
         b7.initializeCorrectHealth(3);
         b7.setCaptainsQuarters(1);
         b7.takeDamage(1);
-        b7.takeDamage(1);
+        assertEquals(0, b7.takeDamage(1));
+        assertEquals(0, b7.getShipHealth());
     }
 
 }
--- src/test/edu/colorado/team6/MineSweeperTest.java
+++ src/test/edu/colorado/team6/MineSweeperTest.java
@@ -2,6 +2,8 @@ package edu.colorado.team6;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MineSweeperTest {
     @Test
     public void canMakeMineSweeper() {
@@ -11,32 +13,34 @@ public class MineSweeperTest {
     @Test
     public void canTakeDamage() {
         MineSweeper mine1 = new MineSweeper();
-        mine1.takeDamage(1);
+        mine1.initializeCorrectHealth(3);
+        assertEquals(0, mine1.takeDamage(1));
     }
 
     @Test
     public void initializeHealthTest() {
         MineSweeper mine2 = new MineSweeper();
-        mine2.initializeCorrectHealth(2);
+        assertEquals(3, mine2.initializeCorrectHealth(3));
     }
 
     @Test
     public void canDisplayTypeName() {
         MineSweeper mine3 = new MineSweeper();
-        mine3.showShipType();
+        assertEquals(Constants.MINESWEEPER, mine3.showShipType());
     }
 
     @Test
     public void canViewHealth() {
         MineSweeper m4 = new MineSweeper();
-        m4.getShipHealth();
+        m4.initializeCorrectHealth(3);
+        assertEquals(3, m4.getShipHealth());
     }
 
     @Test
     public void hasCaptainsQuarters(){
         MineSweeper m5 = new MineSweeper();
         m5.initializeCorrectHealth(3);
-        m5.setCaptainsQuarters(1);
+        assertEquals(1, m5.setCaptainsQuarters(1));
     }
 
     @Test
@@ -44,7 +48,7 @@ public class MineSweeperTest {
         MineSweeper m6 = new MineSweeper();
         m6.initializeCorrectHealth(3);
         m6.setCaptainsQuarters(1);
-        m6.getCaptainsQuarters();
+        assertEquals(1, m6.getCaptainsQuarters());
     }
 
     @Test
@@ -52,6 +56,7 @@ public class MineSweeperTest {
         MineSweeper m7 = new MineSweeper();
         m7.initializeCorrectHealth(3);
         m7.setCaptainsQuarters(1);
-        m7.takeDamage(1);
+        assertEquals(0, m7.takeDamage(1));
+        assertEquals(0, m7.getShipHealth());
     }
 }
