51482749bee5e8d1e4793b0c052298599559e86a
Isaac Vance <isaacchristophervance@gmail.com>
2021-04-14 12:10:11 -0600

    Begin UI, add messed up CSS

--- src/edu/colorado/team6/battleshipGUI.java
+++ src/edu/colorado/team6/battleshipGUI.java
@@ -1,4 +1,23 @@
 package edu.colorado.team6;
 
-public class battleshipGUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class battleshipGUI extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("battleshipGUI.fxml"));
+        Scene scene = new Scene(root, 800, 500);
+        scene.getStylesheets().add("path/stylesheet.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
